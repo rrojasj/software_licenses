0ce4f237328aca94407a058b8392c405b4491ea7
rrojasj <robjimn@amazon.com>
2025-01-31 01:22:16 -0600

    Minimal changes

--- js/login.js
+++ js/login.js
@@ -1,85 +1,103 @@
 "use strict";
 
-const user1 = {
-    fullName: "Diana Pasos",
-    id: "1-1623-1245",
-    userName: "dpasoss",
-    password: "sm123"
+const users = {
+    user1: {
+        fullName: "Diana Pasos",
+        id: "1-1623-1245",
+        userName: "dpasoss",
+        password: "sm123"
+    },
+    user2: {
+        fullName: "Roberto Rojas",
+        id: "1-1290-2346",
+        userName: "rrojasj",
+        password: "sm123"
+    },
+    user3: {
+        fullName: "Jason Corrales",
+        id: "1-1234-5678",
+        userName: "jcorrales",
+        password: "sm123"
+    }
 }
 
-const user2 = {
-    fullName: "Roberto Rojas",
-    id: "1-1290-2346",
-    userName: "rrojasj",
-    password: "sm123"
-}
+// const user1 = {
+//     fullName: "Diana Pasos",
+//     id: "1-1623-1245",
+//     userName: "dpasoss",
+//     password: "sm123"
+// }
 
-const user3 = {
-    fullName: "Jason Corrales",
-    id: "1-1234-5678",
-    userName: "jcorrales",
-    password: "sm123"
-}
+// const user2 = {
+//     fullName: "Roberto Rojas",
+//     id: "1-1290-2346",
+//     userName: "rrojasj",
+//     password: "sm123"
+// }
+
+// const user3 = {
+//     fullName: "Jason Corrales",
+//     id: "1-1234-5678",
+//     userName: "jcorrales",
+//     password: "sm123"
+// }
 
 const loginForm = document.getElementById('login-form');
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    localStorage.setItem("user1", JSON.stringify(user1));
-    localStorage.setItem("user2", JSON.stringify(user2));
-    localStorage.setItem("user3", JSON.stringify(user3));
-
-    // const loginForm = document.getElementById("loginForm");
-    // loginForm.addEventListener("submit", (event) => {
-    //     event.preventDefault();
-    //     const userName = document.getElementById("userName").value;
-    //     const password = document.getElementById("password").value;
-
-    //     const users = [user1, user2, user3];
-    //     const user = users.find((user) => user.userName === userName && user.password === password);
-
-    //     if (user) {
-    //         localStorage.setItem("currentUser", JSON.stringify(user));
-    //         window.location.href = "home.html";
-    //     } else {
-    //         alert("Usuario o contraseña incorrectos");
-    //     }
-    // });
+    localStorage.setItem("users", JSON.stringify(users));
 });
 
 loginForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const userName = document.getElementById("input-username").value;
-    const userPswd = document.getElementById("input-pswd").value;
+    const inputUsername = document.getElementById("input-username");
+    const inputPassword = document.getElementById("input-pswd");
 
+    const isValid = validateRequiredInputs(inputUsername, inputPassword);
+
+    if (isValid) {
+        const users = JSON.parse(localStorage.getItem("users"));
+        const user = users[inputUsername.value];
+        if (user) {
+            if (user.password === inputPassword.value) {
+                localStorage.setItem("currentUser", JSON.stringify(user));
+                window.location.href = "home.html";
+            } else {
+                inputPassword.classList.add('error-input');
+                document.getElementById('error-msg-pswd').textContent = 'contraseña incorrecta';
+            }
+        } else {
+            inputUsername.classList.add('error-input');
+            document.getElementById('error-msg-username').textContent = 'usuario no encontrado';
+        }
+    }
 
 });
 
-function validateLoginInputs() {
+function validateRequiredInputs(inputUsername, inputPassword) {
     let allValid = true;
 
-    const loginFields = [
-        {id: 'input-username', name: 'username'},
-        {id: 'input-pswd', name: 'password'}
-    ];
+    const requiredUserMsg = document.getElementById('error-msg-username');
+    const requiredPswdMsg = document.getElementById('error-msg-pswd');
 
-    loginFields.forEach((field, index) => {
-        const input = document.getElementById(field.id);
-        const inputValue = input?.value.trim();
+    if (inputUsername.value === '') {
+        inputUsername.classList.add('error-input');
+        requiredUserMsg.textContent = 'campo requerido *';
+        allValid = false;
+    } else {
+        inputUsername.classList.remove('error-input');
+        requiredUserMsg.textContent = '';
+    }
+    if (inputPassword.value === '') {
+        inputPassword.classList.add('error-input');
+        requiredPswdMsg.textContent = 'campo requerido *';
+        allValid = false;
+    }
+    return allValid;
+}
 
-        if(!input) {
-            console.error(`El elemento con el ID '${field.id}' no ha sido encontrado`);
-            allValid = false;
-            return;
-        }
-    
-        if (inputValue === '') {}
-    });
+function validateUser(user, pswd) {
 
-
-    
-
-    
 }
 
 
