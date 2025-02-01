afb23dbfc3d9d6bad169df24914336dd02cfadd2
dpasoss <dpasoss@ucenfotec.ac.cr>
2025-01-31 21:26:16 -0600

    created index and added dropdowns

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const licencias = [
+      "MIT", "GPL v3", "Apache 2.0", "BSD 3-Clause", 
+      "Mozilla Public License 2.0", "Creative Commons", "Proprietary"
+    ];
+    const listaLicencias = document.getElementById("listaLicencias");
+    const licenciasSeleccionadas = document.getElementById("licenciasSeleccionadas");
+    const agregarBtn = document.getElementById("agregarBtn");
+
+
+}
